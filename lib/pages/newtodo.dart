4f33474568cb2ade2187b081d3a9c0a37bfe94c0
Josh <josueaugusto@live.com>
2022-03-29 01:19:34 -0300

    Inicio da tela de novo lembrete

--- lib/pages/newtodo.dart
+++ lib/pages/newtodo.dart
@@ -29,6 +29,10 @@ class _NewTodoState extends State<NewTodo> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            buildText('Titulo'),
+            const SizedBox(height: 8),
+            buildText('Descrição'),
+            const SizedBox(height: 8),
             ElevatedButton(
               onPressed: () {},
               style: ElevatedButton.styleFrom(
@@ -58,6 +62,32 @@ class _NewTodoState extends State<NewTodo> {
           ],
         ),
       ),
+    );
+  }
+
+  Widget buildText(String title) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Text(
+            title,
+            style: const TextStyle(
+              fontSize: 13,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+        const SizedBox(height: 2),
+        const TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(borderSide: BorderSide.none),
+            fillColor: Color(0xffefefef),
+            filled: true,
+          ),
+        ),
+      ],
     );
   }
 }
